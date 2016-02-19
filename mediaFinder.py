baf4b07f94bb980ddb5aa70ce45a073f6ee9b85d
Suprabhat Gurrala <supragurrala@gmail.com>
2016-02-19 14:11:56 -0500

    Initial Commit

---
+++ mediaFinder.py
@@ -0,0 +1,13 @@
+import os, re, sys
+fileExt = [".mp4",".jpg",".mp3"] #file extensions to search for
+if (sys.argv[1]):
+    path = sys.argv[1] #checks if a path parameter was passed
+else:
+    path = os.getcwd() #uses current directory if no parameter was passed
+for root, dirs, files in os.walk(path): #loops through all files and subdirectories
+    for name in files:
+        file = os.path.join(root, name)
+        for ext in fileExt:
+            fileType = file[file.rfind("."): len(file)]
+            if (fileType == ext):
+                print file
